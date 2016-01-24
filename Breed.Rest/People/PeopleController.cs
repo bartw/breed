50b53ee66e68a9685a668c66f49c0b4e4ea5d40a
Bart Wijnants <bartwijnants@outlook.com>
2016-01-24 12:54:32 +0100

    added controller

---
+++ Breed.Rest/People/PeopleController.cs
@@ -0,0 +1,54 @@
+﻿using Breed.Business.People;
+using Breed.Contract.People.Dto;
+using Breed.Contract.People.Interface;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Breed.Rest.People
+{
+    [RoutePrefix("api/people")]
+    public class PeopleController : ApiController, IPeopleFacade
+    {
+        private readonly IPeopleFacade _peopleFacade;
+
+        public PeopleController()
+        {
+            _peopleFacade = new PeopleFacade();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<PersonDto> GetAll()
+        {
+            return _peopleFacade.GetAll();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public PersonDto Get(int id)
+        {
+            return _peopleFacade.Get(id);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public PersonDto Create(PersonDto person)
+        {
+            return _peopleFacade.Create(person);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public PersonDto Update(PersonDto person)
+        {
+            return _peopleFacade.Update(person);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            _peopleFacade.Delete(id);
+        }
+    }
+}
